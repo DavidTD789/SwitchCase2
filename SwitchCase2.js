37cc98733f75f7af0790d0a18d419afc83a4fbd7
DavidTD789 <138784445+DavidTD789@users.noreply.github.com>
2023-07-29 13:55:09 +0400

    SwitchCase2

---
+++ SwitchCase2.js
@@ -0,0 +1,25 @@
+let g = prompt("Hello guys - ")
+switch(g){
+    case "1":
+    case "2":
+    case "3":
+        console.log("Dzemer")
+      break 
+    case "4":
+    case "5":
+    case "6":
+        console.log("Garun")
+      break 
+    case "7":
+    case "8":
+    case "9":
+        console.log("Amar")
+      break 
+    case "10":
+    case "11":
+    case "12":
+        console.log("Ashun")
+      break 
+        default:
+        console.log("Error")
+}
